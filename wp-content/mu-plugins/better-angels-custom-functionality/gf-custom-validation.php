bdaaaed1aeac09133741a886bbc4aeaffd88579d
Blake Lundquist <lundqub@outlook.com>
2019-06-06 13:31:16 -0700

    add custom validation for join form b

--- wp-content/mu-plugins/better-angels-custom-functionality/gf-custom-validation.php
+++ wp-content/mu-plugins/better-angels-custom-functionality/gf-custom-validation.php
@@ -1,30 +1,28 @@
-<?php
-add_filter( 'gform_validation', 'custom_validation' );
+<?php 
 
-function custom_validation( $validation_result ) {
+add_filter( 'gform_validation', 'ba_custom_validation' );
+
+function ba_custom_validation( $validation_result ) {
 
   $form = $validation_result['form'];
 
-  if ( $form['id'] != 8 ) {
+  if ( $form['id'] != 22 ) {
     return $validation_result;
   }
 
-    if ( rgpost( 'input_15' ) == 'Yearly Contribution' ) {
+    if ( rgpost( 'input_15' ) == 'Monthly' && rgpost( 'input_48' ) == 0) {
 
-      $donation_value = intval (str_replace('$', '', rgpost( 'input_14' )));
+      $donation_value = intval (str_replace('$', '', rgpost( 'input_49' )));
       //If selected, yearly amount (field 14) must be 12 or greater
-      if ( $donation_value < 12 ) {
+      if ( $donation_value < 1 ) {
 
-          // set the form validation to false
           $validation_result['is_valid'] = false;
 
-          //finding Field with ID of 14 (yearly contribution) and marking it as failed validation
           foreach( $form['fields'] as $field ) {
 
-              //NOTE: replace 1 with the field you would like to validate, 14 in this case
-              if ( $field->id == '14' ) {
+              if ( $field->id == '49' ) {
                   $field->failed_validation = true;
-                  $field->validation_message = 'Yearly contribution must be $12 or more';
+                  $field->validation_message = 'Monthly contribution must be $1 or more';
                   break;
               }
           }
@@ -32,12 +30,12 @@ function custom_validation( $validation_result ) {
       }
   }
 
-  elseif ( rgpost( 'input_15' ) == 'Monthly Contribution' && rgpost( 'input_5' ) == "Other|0") {
+  elseif ( rgpost( 'input_15' ) == 'Yearly' && rgpost( 'input_5' ) == "Other|0") {
 
-      $donation_value = intval (str_replace('$', '', rgpost( 'input_11' )));
+      $donation_value = intval (str_replace('$', '', rgpost( 'input_10' )));
 
       //If selected, "other" monthly amount (field 11) must be 1 or greater
-      if ( $donation_value < 1 ) {
+      if ( $donation_value < 12 ) {
 
         // set the form validation to false
         $validation_result['is_valid'] = false;
@@ -46,9 +44,9 @@ function custom_validation( $validation_result ) {
         foreach( $form['fields'] as $field ) {
 
           //NOTE: replace 1 with the field you would like to validate, 14 in this case
-          if ( $field->id == '11' ) {
+          if ( $field->id == '10' ) {
             $field->failed_validation = true;
-            $field->validation_message = 'Monthly contribution must be $1 or more';
+            $field->validation_message = 'Yearly contribution must be $12 or more';
             break;
           }
         }
