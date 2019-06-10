4c0d5ce24a4c197e3dc9e663b4267fad0325505a
Blake Lundquist <lundqub@outlook.com>
2019-06-10 11:53:42 -0700

    disable default WordPress new user notification emails

--- wp-content/mu-plugins/better-angels-custom-functionality/disable-admin-notifications.php
+++ wp-content/mu-plugins/better-angels-custom-functionality/disable-admin-notifications.php
@@ -7,9 +7,16 @@ if ( ! function_exists( 'gf_new_user_notification' ) ) {
   }
 }
 
+//disable default WordPress new user notification emails
+if (!function_exists('wp_new_user_notification')) {
+  function wp_new_user_notification( $user_id, $deprecated = null, $notify = '' ) {
+    return;
+  }
+}
+
 //disable default WordPress change password notifications
 if (!function_exists('wp_password_change_notification')) {
-    function wp_password_change_notification($user) {
+  function wp_password_change_notification($user) {
     return;
-    }
+  }
 }
