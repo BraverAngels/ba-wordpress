10a10ec34055ced2809eab430dac393d80e73c40
Blake Lundquist <lundqub@outlook.com>
2019-06-12 09:55:55 -0700

    alter disable password change notification code

--- wp-content/mu-plugins/better-angels-custom-functionality/disable-admin-notifications.php
+++ wp-content/mu-plugins/better-angels-custom-functionality/disable-admin-notifications.php
@@ -15,8 +15,6 @@ if (!function_exists('wp_new_user_notification')) {
 }
 
 //disable default WordPress change password notifications
-if (!function_exists('wp_password_change_notification')) {
-  function wp_password_change_notification($user) {
-    return;
-  }
+if ( !function_exists( 'wp_password_change_notification' ) ) {
+    function wp_password_change_notification() {}
 }
