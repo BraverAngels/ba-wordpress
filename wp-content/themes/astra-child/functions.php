44dde5e54ab4d6b9a66c025b5c7d2338af86f27b
Blake Lundquist <lundqub@outlook.com>
2019-06-14 10:22:40 -0700

    Redirect single Organizer pages to main events list

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -18,8 +18,25 @@ define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
  */
 function child_enqueue_styles() {
 
-	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
+  wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
 
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+/**
+ * Redirect single Organizer pages to main events list
+ */
+add_action( 'template_redirect', 'redirect_organizer_and_venue_type_single' );
+  function redirect_organizer_and_venue_type_single() {
+      if ( ! is_singular( 'tribe_organizer' ) )
+          return;
+      if ( tribe_get_events_link() ) {
+        wp_redirect( tribe_get_events_link(), 301 );
+        exit;
+      } else {
+        wp_redirect( home_url(), 301 );
+        exit;
+      }
+
+  }
