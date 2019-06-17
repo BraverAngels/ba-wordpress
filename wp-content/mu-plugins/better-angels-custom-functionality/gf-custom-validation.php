f4f1134f957af1c7ce25cec2504bea795934cb05
Blake Lundquist <lundqub@outlook.com>
2019-06-17 11:19:01 -0700

    fix syntax error

--- wp-content/mu-plugins/better-angels-custom-functionality/gf-custom-validation.php
+++ wp-content/mu-plugins/better-angels-custom-functionality/gf-custom-validation.php
@@ -53,7 +53,7 @@ function ba_custom_validation( $validation_result ) {
       }
   }
 
-  elseif ( rgpost( 'input_15' ) == 'One Time Gift' {
+  elseif ( rgpost( 'input_15' ) == 'One Time Gift' ) {
 
       $donation_value = intval (str_replace('$', '', rgpost( 'input_50' )));
 
