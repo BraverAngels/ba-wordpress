f2aa3f07481c85698c27af81de5b5a4e6a7e09da
Blake Lundquist <lundqub@outlook.com>
2019-06-18 11:32:27 -0700

    download jquery from CDN

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -13,6 +13,25 @@
  */
 define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
 
+add_action('init', 'use_jquery_from_google');
+
+function use_jquery_from_google () {
+  if (is_admin()) {
+    return;
+  }
+
+  global $wp_scripts;
+  if (isset($wp_scripts->registered['jquery']->ver)) {
+    $ver = $wp_scripts->registered['jquery']->ver;
+                $ver = str_replace("-wp", "", $ver);
+  } else {
+    $ver = '1.12.4';
+  }
+
+  wp_deregister_script('jquery');
+  wp_register_script('jquery', "https://ajax.googleapis.com/ajax/libs/jquery/$ver/jquery.min.js", false, $ver);
+}
+
 /**
  * Enqueue styles
  */
