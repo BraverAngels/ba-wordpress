dcf9d7d4d0def5ddfab224f284bebd2f4809ffed
Blake Lundquist <lundqub@outlook.com>
2019-07-03 13:28:21 -0700

    add custom login form styles

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -59,3 +59,32 @@ add_action( 'template_redirect', 'redirect_organizer_and_venue_type_single' );
       }
 
   }
+
+  function ba_login_page_styles() {
+  ?>
+    <style type="text/css">
+      body.login div#login h1 a {
+        background-image: url(https://43n9a3188zqw4cdz825bl1q4-wpengine.netdna-ssl.com/wp-content/uploads/2018/11/Better-Angels-clear-logo.png);
+        padding-bottom: 0;
+        width: 100%;
+        background-size: 250px;
+        background-position: bottom;
+      }
+      body.login #login_error, .login .message, .login .success {
+        border-left: 4px solid #c80000 !important;
+      }
+      body.login input#wp-submit {
+        background: #2850a0;
+        border-color: #2850a0;
+        box-shadow: 0 1px 0 #2850a0;
+        color: #fff;
+        text-decoration: none;
+        text-shadow: none;
+      }
+      body.login input#wp-submit:focus, body.login input#wp-submit:hover {
+        background: #2b4997;
+        border-color: #2b4997;
+      }
+    </style>
+ <?php
+} add_action( 'login_enqueue_scripts', 'ba_login_page_styles' );
