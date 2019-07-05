eedb9401e68c9b554535aa67cbf153578ebbc8c8
Blake Lundquist <lundqub@outlook.com>
2019-07-05 12:39:05 -0700

    fix plugin info

--- wp-content/mu-plugins/better-angels-custom-functionality.php
+++ wp-content/mu-plugins/better-angels-custom-functionality.php
@@ -3,12 +3,12 @@
    Plugin Name: Better Angels Custom Functionality
    description: Custom Features for the Better Angels website
    Version: 1.0.0
-   Author: Blake Eric
-   Author URI: https://blakeeric.com
+   Author: Blake Lundquist
+   Author URI: https://blakelundquist.dev
    License: GPL2
    */
 
 
    require_once(__DIR__ . '/better-angels-custom-functionality/better-angels-custom-functionality.php');
-   
+
 ?>
