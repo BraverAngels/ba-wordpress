e5fee5c236adf9954c448d0f98ec20df54ebd244
Blake Lundquist <lundqub@outlook.com>
2019-07-25 14:09:32 -0700

    add default gravitar

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -120,3 +120,14 @@ function inline_scripts() {
     <?php
 }
 add_action( 'wp_enqueue_scripts', 'inline_scripts', 1, 1 );
+
+
+//Set Default Gravatar
+
+add_filter( 'avatar_defaults', 'ba_new_gravatar' );
+
+function ba_new_gravatar ($avatar_defaults) {
+  $myavatar = 'https://www.better-angels.org/wp-content/uploads/2018/08/BA_Logo-1-150x150.png';
+  $avatar_defaults[$myavatar] = "Default Gravatar";
+  return $avatar_defaults;
+}
