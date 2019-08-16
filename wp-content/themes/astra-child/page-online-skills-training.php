cfbfa85ec5d877502741b615f7ce0de39be41394
Blake Lundquist <lundqub@outlook.com>
2019-08-16 11:07:11 -0700

    adjust skills course iframe height

--- wp-content/themes/astra-child/page-online-skills-training.php
+++ wp-content/themes/astra-child/page-online-skills-training.php
@@ -4,18 +4,17 @@ get_header(); ?>
     <iframe src="https://www.better-angels.org/wp-content/skills-training/" width="100%" height="100%"></iframe>
   </div>
   <style>
+    header#masthead, .elementor-567.elementor-location-header, .elementor-570.elementor-location-footer {
+      display: none !important;
+    }
     #skills-training-wrapper {
       background: white;
-      height: 100vh;
-      position: fixed;
-      left: 0;
-      top: 0;
-      width: 100vw;
-      z-index: 10000;
+      height: 91vh;
     }
-    #skills-training-wrapper iframe {
-      height: 100%;
-      width: 100%;
+    .ast-container {
+      padding: 0;
+      margin: 0;
+      max-width: 100%;
     }
   </style>
 
