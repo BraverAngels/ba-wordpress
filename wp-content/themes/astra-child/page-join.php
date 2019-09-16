3a7a922c577d41898f767f0f29e765d841a3dbcc
Blake Lundquist <lundqub@outlook.com>
2019-09-16 14:09:13 -0700

    fonts

--- wp-content/themes/astra-child/page-join.php
+++ wp-content/themes/astra-child/page-join.php
@@ -17,12 +17,12 @@ get_header(); ?>
 
   <div class="entry-content clear" itemprop="text">
     <div class="join-content-column for-small">
-      <h2><?php the_title(); ?></h2>
-      <h3 style="color: gray; font-family: 'Karla', sans-serif;"><em>Become a Member Today!</em></h3>
+      <h2 style="font-family: 'Nunito Sans', 'Karla';"><?php the_title(); ?></h2>
+      <h3 style="color: gray; font-family: 'Nunito Sans', 'Karla';"><em>Become a Member Today!</em></h3>
     </div>
     <div class="join-content-column for-large">
-      <h2><?php the_title(); ?></h2>
-      <h3 style="color: gray; font-family: 'Karla', sans-serif;"><em>Become a Member Today!</em></h3>
+      <h2 style="font-family: 'Nunito Sans', 'Karla';"><?php the_title(); ?></h2>
+      <h3 style="color: gray; font-family: 'Nunito Sans', 'Karla';"><em>Become a Member Today!</em></h3>
       <?php the_content(); ?>
     </div>
     <div class="join-selection-column">
