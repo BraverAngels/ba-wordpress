c09f7f2e29cd7257aad39dd157e41abe3e6a54f2
Blake Lundquist <lundqub@outlook.com>
2019-09-18 10:53:11 -0700

    separate libray sidebar categories, update 'how to right a review' link url

--- wp-content/themes/astra-child/template-parts/content-library-sidebar.php
+++ wp-content/themes/astra-child/template-parts/content-library-sidebar.php
@@ -1,12 +1,20 @@
 <div class="library-sidebar-inner">
   <?php
+  $ba_readings = get_terms( array(
+      'taxonomy' => 'library_category',
+      'hide_empty' => true,
+      'include' => 3082
+  ) );
+
   $terms = get_terms( array(
       'taxonomy' => 'library_category',
       'hide_empty' => true,
+      'exclude' => 3082
   ) ); ?>
   <ul class="library-labels-list">
-    <?php foreach($terms as $term) : ?>
-      <li>
+
+    <?php foreach($ba_readings as $term) : ?>
+      <li class="library-labels-ba-readings">
         <a
           <?php if (is_tax('library_category', $term->name) ): ?>
             class="currently-selected"
@@ -16,5 +24,23 @@
         </a>
       </li>
     <?php endforeach; ?>
+
+    <li class="library-recommended-readings">
+      <span class="library-recommended-readings-header">Member-Recommended Readings<i class="fa fa-chevron-down ba-dropdown-icon" aria-hidden="true"></i></span>
+      <ul>
+        <?php foreach($terms as $term) : ?>
+          <li>
+            <a
+              <?php if (is_tax('library_category', $term->name) ): ?>
+                class="currently-selected"
+              <?php endif; ?>
+              href="<?php echo get_term_link( $term, 'library_category'); ?>">
+              <?php echo $term->name; ?>
+            </a>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    </li>
+
   </ul>
 </div>
