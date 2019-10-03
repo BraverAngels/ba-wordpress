c9e17706a5796e2f16fe5a37f0ea9cf28d71824a
Blake Lundquist <lundqub@outlook.com>
2019-10-03 14:23:22 -0700

    fix bg image positioning

--- wp-content/themes/astra-child/archive-library.php
+++ wp-content/themes/astra-child/archive-library.php
@@ -10,7 +10,7 @@
 
 get_header(); ?>
 
-  <section class="ast-archive-description" style="text-align:center;border-radius: 3px;padding: 3rem 1.25rem;color: white;background:#23356c;background-image: url(https://www.better-angels.org/wp-content/uploads/2019/09/lincolnbooks1.jpg); background-size:cover;background-position:center 20%;">
+  <section class="ast-archive-description" style="text-align:center;border-radius: 3px;padding: 3rem 1.25rem;color: white;background:#23356c;background-image: url(https://www.better-angels.org/wp-content/uploads/2019/09/lincolnbooks1.jpg); background-size:cover;background-position:center center;">
     <h1 class="page-title ast-archive-title" style="color:white;">Better Angels Library</h1>
     <p>Member-recommended books and resources to encourage dialogue and promote mutual understanding</p>
   </section>
