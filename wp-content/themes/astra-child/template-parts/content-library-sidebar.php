ae271b1d1f579904f5ee5c4fc2423482d221f65b
Blake Lundquist <lundqub@outlook.com>
2019-10-07 12:29:57 -0700

    reverse the order of buttons in library sidebar

--- wp-content/themes/astra-child/template-parts/content-library-sidebar.php
+++ wp-content/themes/astra-child/template-parts/content-library-sidebar.php
@@ -8,11 +8,11 @@
         'title_li' => ''
     ) ); ?>
   </ul>
-  <a href="<?php echo home_url('members-portal/online-book-discussions'); ?>" class="ba-cta-button ba-cta-button-white" style="font-weight: 400; text-align: center; display: block; margin: 0; margin-top: 1rem;font-size: 80%;padding-left:.25rem!important; padding-right: .25rem !important;">
-    Submit a Book or Review
-  </a>
   <a href="<?php echo home_url('how-to-review/'); ?>" class="ba-cta-button ba-cta-button-red" style="font-weight: 400; text-align: center; display: block; margin: 0; margin-top: 1rem;font-size: 80%;padding-left:.25rem!important; padding-right: .25rem !important;">
     Online Book Discussions
+  </a>
+  <a href="<?php echo home_url('members-portal/online-book-discussions'); ?>" class="ba-cta-button ba-cta-button-white" style="font-weight: 400; text-align: center; display: block; margin: 0; margin-top: 1rem;font-size: 80%;padding-left:.25rem!important; padding-right: .25rem !important;">
+    Submit a Book or Review
   </a>
   <span class="library-sidebar-contact-info">
     Questions, concerns and book recommendations for the Better Angels Library can be submitted to <a href="mailto:mailto:library@better-angels.org">library@better-angels.org</a>
