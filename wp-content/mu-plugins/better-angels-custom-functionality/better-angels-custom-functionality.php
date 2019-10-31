bd56ed8675dcbd3fd349e7a6a0f8f080bc814aac
Blake Lundquist <lundqub@outlook.com>
2019-10-31 12:19:31 -0700

    remove unnecessary mu plugin files

--- wp-content/mu-plugins/better-angels-custom-functionality/better-angels-custom-functionality.php
+++ wp-content/mu-plugins/better-angels-custom-functionality/better-angels-custom-functionality.php
@@ -11,8 +11,6 @@
    require_once(__DIR__ . '/fb-pixel.php');
    require_once(__DIR__ . '/gf-action-network.php');
    require_once(__DIR__ . '/gf-custom-validation.php');
-   require_once(__DIR__ . '/venue-states-select-field.php');
-   require_once(__DIR__ . '/gf-conditional-user-registration.php');
    require_once(__DIR__ . '/mepr-hooks.php');
    require_once(__DIR__ . '/disable-admin-notifications.php');
    require_once(__DIR__ . '/stripe-webhook.php');
