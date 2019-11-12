16557ac54b5e71f55ff57bda4b16709f49b46b04
Blake Lundquist <lundqub@outlook.com>
2019-11-11 16:41:54 -0800

    make editors and admins exempt from manual library review approval

--- wp-content/mu-plugins/better-angels-custom-functionality/library-comments.php
+++ wp-content/mu-plugins/better-angels-custom-functionality/library-comments.php
@@ -1,7 +1,7 @@
 <?php
 // Settings to modify comment functionality specifically for "Library" post type.
 
-//Make sure the right email gets notified when a review (comment) is made on a library item.
+// Make sure the right email gets notified when a review (comment) is made on a library item.
 function ba_library_comment_moderation_recipients( $emails, $comment_id ) {
   $comment = get_comment( $comment_id );
 
@@ -17,15 +17,23 @@ function ba_library_comment_moderation_recipients( $emails, $comment_id ) {
 add_filter( 'comment_moderation_recipients', 'ba_library_comment_moderation_recipients', 11, 2 );
 add_filter( 'comment_notification_recipients', 'ba_library_comment_moderation_recipients', 11, 2 );
 
+
+// Make sure that all library reviews made by non-admins must be manually approved
 function ba_unapprove_library_review( $approved , $commentdata ) {
 
-    $comment_post_type = get_post_type( $commentdata['comment_post_ID'] );
+  $comment_post_type = get_post_type( $commentdata['comment_post_ID'] );
+  $comment_user_roles = get_user_roles( $commentdata['user_id'] );
 
-    if ($comment_post_type == 'library') {
-      $approved = 0;
-    }
+  if (
+    $comment_post_type == 'library'
+    && !in_array( 'editor', $comment_user_roles )
+    && !in_array( 'administrator', $comment_user_roles )
+  ) {
+    $approved = 0;
+  }
 
-    return $approved;
+  return $approved;
+
 }
 
 add_filter( 'pre_comment_approved' , 'ba_unapprove_library_review' , '99', 2 );
