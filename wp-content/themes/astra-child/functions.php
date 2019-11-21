46ce3729c30f9e7104478640bee3c14fefe901d7
Blake Lundquist <lundqub@outlook.com>
2019-11-20 16:38:14 -0800

    fix library horizontal scrolling issue

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.19' );
+define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.20' );
 
 add_action('wp_enqueue_scripts', 'use_jquery_from_google');
 
