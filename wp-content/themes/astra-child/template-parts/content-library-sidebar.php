dfe2144b642dfb3f152cf7a5aacbeddcb32b9fad
Blake Lundquist <lundqub@outlook.com>
2020-04-15 13:51:45 -0700

    fix library email

--- wp-content/themes/astra-child/template-parts/content-library-sidebar.php
+++ wp-content/themes/astra-child/template-parts/content-library-sidebar.php
@@ -24,7 +24,7 @@
   <p class="library-sidebar-contact-info">
     The Braver Angels Library is curated and maintained by members <strong>Bridget Kraft</strong>, <strong>Jennifer Livingston</strong>, and <strong>Bill Roos</strong>.
     <br/>
-    Questions, concerns and book recommendations for the Braver Angels Library can be submitted to <a href="mailto:mailto:library@better-angels.org">library@better-angels.org</a>.
+    Questions, concerns and book recommendations for the Braver Angels Library can be submitted to <a href="mailto:library@braverangels.org">library@braverangels.org</a>.
   </p>
   <p class="library-sidebar-contact-info">
     You can support Braver Angels when you purchase books on <a href="<?php echo home_url('support-better-angels-with-amazon-smile'); ?>">Amazon Smile</a>.
