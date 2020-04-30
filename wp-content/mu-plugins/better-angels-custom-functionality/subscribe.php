2f16e41293e594154a51fd575198f25b2cd394bc
Blake Lundquist <lundqub@outlook.com>
2020-04-30 12:27:42 -0700

    add subscribe form shortcode

--- wp-content/mu-plugins/better-angels-custom-functionality/subscribe.php
+++ wp-content/mu-plugins/better-angels-custom-functionality/subscribe.php
@@ -1,9 +1,30 @@
 <?php
 
-function subscribe_form_shortcode() {
-  return do_shortcode( '[gravityform id="32" title="false" description="false" ajax="true" tabindex="49"]' );
+function subscribe_form_shortcode($atts = '') {
+
+  $value = shortcode_atts( array(
+    'title' => '',
+    'description' => '',
+  ), $atts );
+
+  $html = '<div class="subscribeForm-wrapper">';
+
+    if ($value['title']) {
+      $html .= '<h3 class="subscribeForm-title">' . $value['title'] . '</h3>';
+    }
+
+    if ($value['description']) {
+      $html .= '<p class="subscribeForm-description">' . $value['description'] . '</p>';
+    }
+
+    $html .= do_shortcode( '[gravityform id="33" title="false" description="false" ajax="true" tabindex="49"]' );
+
+  $html .= '</div>';
+
+  return $html;
 }
 
 add_shortcode('subscribe', 'subscribe_form_shortcode');
+
 
 ?>
