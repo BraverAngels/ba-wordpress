bf029e8bf936b4c6f171b5f3d809ca8afda35a3b
Blake Lundquist <lundqub@outlook.com>
2020-05-07 09:52:37 -0700

    remove stripe webhook (moved to external api)

--- wp-content/mu-plugins/better-angels-custom-functionality/better-angels-custom-functionality.php
+++ wp-content/mu-plugins/better-angels-custom-functionality/better-angels-custom-functionality.php
@@ -12,7 +12,6 @@
    require_once(__DIR__ . '/gf-action-network.php');
    require_once(__DIR__ . '/mepr-hooks.php');
    require_once(__DIR__ . '/disable-admin-notifications.php');
-   require_once(__DIR__ . '/stripe-webhook.php');
    require_once(__DIR__ . '/library-comments.php');
    require_once(__DIR__ . '/gf-allow-form-total-conditional.php');
    require_once(__DIR__ . '/subscribe.php');
