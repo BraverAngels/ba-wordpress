0b1a180b166fd0b054b66dbb32f9e3a33d477262
Andrew Stillman <andrew@braverangels.org>
2021-03-23 22:14:33 -0400

    Testing new Age field

--- wp-content/mu-plugins/braver-angels-mods/memberpress.php
+++ wp-content/mu-plugins/braver-angels-mods/memberpress.php
@@ -276,9 +276,9 @@ function send_updated_user_data_to_action_network($data){
   if (isset($data['mepr_which_of_the_following_would_best_describe_where_you_live']) && $data['mepr_which_of_the_following_would_best_describe_where_you_live']) {
       $custom_fields['Urban / Rural'] = $data['mepr_which_of_the_following_would_best_describe_where_you_live'];
   }
-  print_r($data);
+
   if (isset($data['mepr_age']) && $data['mepr_age']) {
-      error_log ($data['mepr_age']);
+      print_r($data['mepr_age']);
       $custom_fields['Age'] = $data['mepr_age'];
   }
 
