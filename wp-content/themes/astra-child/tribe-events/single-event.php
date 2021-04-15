93ca0e95a838019fe30eabb8c04c3bf1fa4cdeb0
Andrew Stillman <andrew@braverangels.org>
2021-04-15 11:35:43 -0400

    Testing signup link

--- wp-content/themes/astra-child/tribe-events/single-event.php
+++ wp-content/themes/astra-child/tribe-events/single-event.php
@@ -23,9 +23,9 @@ $event_id = get_the_ID();
 // Signup link
 $signup_link = tribe_get_event_website_link($event_id);
 
-if isset($signup_link) {
+if $signup_link {
     preg_match_all('#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#', $signup_link, $matches);
-    if (isset($matches[0]) && isset($matches[0][0])) {
+    if ($matches[0]) && $matches[0][0]) {
       $signup_link = trim($matches[0][0]);
     }
 }
